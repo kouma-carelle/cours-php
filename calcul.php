3d6b2eb0c70b7aca84dc2610b2431157f52b7dba
kouma-carelle <144114754+kouma-carelle@users.noreply.github.com>
2023-09-16 15:36:33 +0000

    opperation

---
+++ calcul.php
@@ -0,0 +1,12 @@
+<?php
+$somme = 2 + 3;
+$difference = 5 - 2;
+$produit = 4 * 6;
+$quotient = 8 / 2;
+
+//commentaire pour calculer les operations
+echo "la somme de 2+3  = $somme";
+echo "le produit de 4*6 = $produit";
+echo "la difference de 5-2 = $difference ";
+echo " le quotient de 8/2 = $quotient ";
+?>
