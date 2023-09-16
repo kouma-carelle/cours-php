dd8d6c2ca481c318421ad26f93cff931432568af
kouma-carelle <144114754+kouma-carelle@users.noreply.github.com>
2023-09-16 15:00:05 +0000

    definition de table declaration de variable

---
+++ types-de-donnees.php
@@ -0,0 +1,18 @@
+<?php
+//$entier = 42;
+//$flottant = 3.5;
+//$chaine = "Hello, world!" ;
+//$tableau = [1, 2, 3];
+//$booleen = true;
+
+//echo nous permet d'afficher une valeurd de sortir dans la consone
+ //echo "voici un exemple d'entier:$entier";
+ //echo "voici un exemple de flottant:$flottant";
+ //echo "voici un exemple de chaine:$chaine";
+ //echo "voici un exemple de booleen:$booleen";
+ //echo $tableau[0];
+ $moyenne = [ "jean", "atta", "anselme","bkou", "zanzan", "123456789", "ingenieur logiciel", 42];
+
+echo "je suis $moyenne[0] $moyenne[1] $moyenne[2], je reside a $moyenne[3], precisement au $moyenne[4], je suis $moyenne[6], j'ai $moyenne[7],et voici mon numero$moyenne[5]";
+
+?>
---
+++ variable.php
@@ -0,0 +1,6 @@
+<?php
+$nom = "john";
+echo "je suis john n";
+$age = 30;
+echo "j'ai $age ans";
+?>
